570cfa117336c59c3856b1961dc0d3276fc0d0ce
michaelveloso <calabiyau@gmail.com>
2017-09-28 09:29:06 -0400

    Adding ability to read input players from a local csv file

---
+++ GeneralPosition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StratGatherer
+{
+    /// <summary>
+    /// Denotes either a batter or pitcher.
+    /// </summary>
+    public enum GeneralPosition
+    {
+        Batter,
+        Pitcher
+    }
+}
